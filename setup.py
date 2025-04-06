a53869b692ada67e8c56c690aba0b27e1311c1aa
xiXRedNomadXix <best.thomas0909@gmail.com>
2025-04-06 16:19:43 -0400

    Restructure project for pipx support and CLI entry point

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='cane-pole',
-    version='0.1',
-    py_modules=['cane_pole'],
+    name="cane-pole",
+    version="0.1",
+    packages=find_packages(),
     install_requires=[
-        'scapy',
-        'pyroute2',
+        "scapy",
+        "pyroute2",
     ],
     entry_points={
-        'console_scripts': [
-            'cane-pole=cane_pole:main',
+        "console_scripts": [
+            "cane-pole=cane_pole.main:run",
         ],
     },
 )
